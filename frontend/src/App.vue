5cd987bca2e77b6da6753e8ce2f88e9e922a8cfe
Emphyr00 <emphyr00@gmail.com>
2023-06-05 21:01:41 +0000

    Basic landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,56 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
+  <div class="container">
+    <h1 class="title">Fake News Detector</h1>
+    <hr>
+    <div class="constainer menu-container">
+      <div class="menu-element">
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      </div>
+      <div class="menu-element">
+        <RouterLink to="login">Login</RouterLink>
+      </div>
+      <div class="menu-element">
+        <RouterLink to="register">Register</RouterLink>
+      </div>
+      <div class="menu-element">
+        <RouterLink to="about">About</RouterLink>
+      </div>
+      <div class="menu-element" style="margin-left: auto;">
+        <RouterLink to="profile">Profile</RouterLink>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.title {
+  font-size: 80px;
+  font-weight: 3;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.menu-container {
+  margin-top: 50px;
+  display: flex;
+  gap: 20px;
+  font-size: 30px;
+  margin-bottom: 100px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+.menu-element {
+  max-width: 20%;
   text-align: center;
-  margin-top: 2rem;
+  flex-grow: 1;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.menu-element:hover {
+  transition: background-color 1s;
+  background-color: darkgray;
+  color: black
 }
 </style>
+
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Insert text to check if it's validity</h1>
+    <textarea></textarea>
+    <div style="display: flex">
+      <button>Check</button>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+textarea {
+  margin-top: 20px;
+  width: 100%;
+  height: 200px;
+  /* Adjust this to the desired fixed height */
+  background-color: darkgray;
+  color: black;
+  border: none;
+  /* Optional: Removes the default textarea border */
+  padding: 10px;
+  /* Optional: Adds some padding around the text */
+  resize: none;
+  /* Optional: Prevents resizing of the textarea */
+}
+
+button {
+  margin-left: auto;
+  display: inline-block;
+  padding: 10px 20px;
+  text-align: center;
+  border: 1px solid white;
+  border-radius: 0;
+  background-color: transparent;
+  color: white;
+}
+
+button:hover {
+  transition: background-color 1s;
+  background-color: darkgray;
+  color: black;
+}
+</style>
