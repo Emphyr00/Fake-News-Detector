fa8000950d7d12808badfe7bd5f6b7044c0cbf03
jploszaj <jan.ploszaj@outlook.com>
2023-06-07 15:43:59 +0200

    Changed postgres version to 12

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-mgo)#3ak7n(@pmig)q%gtee(imf1sg8n7!ems@r0j4bh#^s)7j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
@@ -75,10 +74,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'fake_news_detector_user_data',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'example',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
--- backend/config/fakenewsdetectorcore/models.py
+++ backend/config/fakenewsdetectorcore/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    history = models.OneToOneField('UserHistory', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+
+class UserHistory(models.Model):
+    def __str__(self):
+        return f"UserHistory {self.pk}"
+
+
+class UserHistoryEntry(models.Model):
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user_history = models.ForeignKey(
+        UserHistory,
+        on_delete=models.CASCADE,
+        related_name='entries',
+        related_query_name='entry',
+    )
+
+    def __str__(self):
+        return self.text
