86829868d968a0dcda9fa1d17ece0982b9a2d6bd
wiktoria-sarniak <wiktoriasarniak@gmail.com>
2024-01-18 23:28:16 +0100

    added ionic

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,24 @@
 import './assets/main.css'
 
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+
+import '@ionic/core/css/ionic.bundle.css';
+
+import { IonicVue } from '@ionic/vue';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,6 +27,11 @@ const app = createApp(App)
 
 app.config.globalProperties.$token = null;
 
+app.use(IonicVue)
 app.use(router)
 
-app.mount('#app')
+//app.mount('#app')
+router.isReady().then(() => {
+    app.mount('#app');
+});
+
