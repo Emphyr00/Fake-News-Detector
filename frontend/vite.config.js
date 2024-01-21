08005f21213ec8fcf4c6d2557a8a8d4d8f2f179f
wiktoria229994 <wiktoriasarniak@gmail.com>
2024-01-21 22:55:00 +0100

    pwa app passing lighthouse test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,47 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
+import mkcert from'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({ registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico'],
+    manifest: {
+      name: 'Fake News Detector',
+      short_name: 'fakeNews',
+      description: 'An app to detect fake news',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: 'pwa-fakenews.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-fakenews192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          "src": "maskable_icon.png",
+          "sizes": "196x196",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    }
+  }),
+  mkcert()
+],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
-    port: 8080
+    port: 8080,
+    https: true
   },
 })
